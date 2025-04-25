b97a9b024b9cfd6017787ded37bee803eaf27839
KuchiMercy <durumercy206@gmail.com>
2025-04-25 12:40:53 +0100

    implemented a new post endpoint

--- consumers-api/src/routes/consumer.routes.js
+++ consumers-api/src/routes/consumer.routes.js
@@ -7,6 +7,34 @@ const microServiceConnector = require("../utils/nodefetch");
 module.exports = () => {
   const api = Router();
 
+  api.post("/", async (req, res) => {
+    try {
+      const {
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        password,
+      } = req.body;
+
+      const saltRounds = 10;
+      const newPassword = await bcrypt.hash(password, saltRounds);
+
+      let response = await consumerController.createConsumer(
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        newPassword
+      );
+      res.status(200).json({ response: true, payload: response });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
+
   api.post("/ratings", async (req, res) => {
     try {
       const {
